d89a2d46181be10136f82aed9e05af5a4efe5fcc
coleforestal <cforestal@outlook.com>
2022-09-06 13:33:37 -0400

    Add project files.

---
+++ Forestal Lab 3 CGT 215.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+void printMenu(int& choice) {
+
+	cout << "Welcome to my program, please select an operation to perform:" << endl;
+	cout << "1. Addition" << endl;
+	cout << "2. Subtraction" << endl;
+	cout << "3. Multiplication" << endl;
+	cout << "4. Division" << endl;
+	cout << "\nYour Selection: ";
+	cin >> choice;
+}
+void getChoices(float& A, float& B) {
+
+	cout << "Please enter the first value:";
+
+	cin >> A;
+
+	// The rest of this function is an exercise to the reader
+}
+void firstChoice(float A, float B) {
+
+	cout << "This is proving the first choice ran" << endl;
+}
+void secondChoice(float A, float B) {
+
+	cout << "This is proving the second choice ran" << endl;
+}
+int main() {
+
+	int choice;
+	float A;
+	float B;
+
+	printMenu(choice);
+
+	getChoices(A, B);
+
+	if (choice == 1) {
+		firstChoice(A, B);
+
+	}
+	if (choice == 2) {
+		secondChoice(A, B);
+	}
+	return 0;
+}
+
